ba0594792c9d5daaca7e30d639ff1965094e505d
381706-1VlasovAndrey <andrio.vlasov.2000@mail.ru>
2022-06-30 20:07:21 +0300

    Initial commit

---
+++ app/src/main/java/ru/vlasov/newsapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.vlasov.newsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
